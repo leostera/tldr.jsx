eaa57d2b430123b67e59d856bf08fd397039a170
Leandro Ostera <leandro@ostera.io>
2016-01-09 20:48:21 +0100

    Fixes #11. finally some ca(t)ching that works

--- src/actions/Command.js
+++ src/actions/Command.js
@@ -13,31 +13,70 @@ let search = (name) => {
 };
 
 let requestIndex = function *() {
-  let requestOptions = {
+  let response = yield request(requestOptions());
+  if(response.status === 0) {
+    throw response;
+  }
+  return response;
+};
+
+let requestOptions = () => {
+  let opts = {
     method: 'GET',
     url: INDEX_URL,
     withCredentials: false
   };
 
-  let modifiedSince = localStorage.getItem("tldr/index.cache");
-  // Yes, that's right, it's the string "undefined"...gee.
-  if(modifiedSince !== "undefined" && modifiedSince !== undefined) {
-    requestOptions.headers = {
+  let modifiedSince = hasModifiedSince();
+  if(modifiedSince) {
+    opts.headers = {
       'If-Modified-Since': modifiedSince
     };
   }
 
-  let response = yield request(requestOptions);
-  return response;
-};
+  return opts;
+}
+
+let hasModifiedSince = () => {
+  let expiry = localStorage.getItem("tldr/index.expiry");
+  // Yes, that's right, it's the string "undefined"...gee.
+  // So if it's truthy (not null, not undefined) it works
+  if( expiry && expiry !== "undefined" ) {
+    return expiry;
+  } else {
+    return false;
+  }
+}
 
 let getIndex = () => {
-  return Rx.Observable.spawn(requestIndex).tap( (response) => {
-      let ifModifiedSince = response.headers["last-modified"];
-      localStorage.setItem("tldr/index.cache", ifModifiedSince);
-      let commands = response.data.commands;
-      localStorage.setItem("tldr/index", JSON.stringify(commands));
-  }).flatMap( res => res.data.commands )
+  return Rx.Observable.spawn(requestIndex)
+    .tap(cache)
+    .flatMap(flatten)
+    .catch(localIndex)
+};
+
+let localIndex = (e) => {
+  let raw = localStorage.getItem('tldr/index') || "[]";
+  let commands = JSON.parse(raw) || [];
+  return Rx.Observable.fromArray(commands);
+};
+
+let flatten = (res) => {
+  let hasData = typeof res.data === "object";
+  if (hasData && res.data.commands) {
+    return res.data.commands;
+  } else {
+    return res;
+  }
+};
+
+let cache = (response) => {
+  if(response && response.headers && response.data ) {
+    let ifModifiedSince = response.headers["last-modified"];
+    localStorage.setItem("tldr/index.expiry", ifModifiedSince);
+    let commands = JSON.stringify(response.data.commands);
+    localStorage.setItem("tldr/index", commands);
+  }
 }
 
 let Command = {
