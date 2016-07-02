3fa62b3fe63eaa4a81226c28cd763c07aa6d8e2d
Leandro Ostera <leandro@ostera.io>
2016-07-03 01:39:18 +0200

    Copy change

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -15,11 +15,12 @@ const Welcome = () => (
       <p> See the input box by the logo? Just type in a command and see the magic happen! </p>
       <p> Try <code>osx/say</code>, <code>linux/du</code>, or simply <code>man</code>. </p>
       <p> Some commands are widely available with the same interface, some other have variants per operating system. Currently the <code>tldr-pages</code> project splits comman into 4 categories: common, linux, osx, and sunos.</p>
+      <p> <code>du</code>, for example, is available under both <code>linux</code> and <code>osx</code>. </p>
       <h3> What is <Tldr size="small" />? </h3>
       <p> This is a web client for a project called <code>tldr-pages</code>; they are a community effort to simplify the beloved man pages with practical examples. </p>
       <p> Nifty indeed. </p>
       <p> Read more and join the tldr wagon at <Link href="https://tldr-pages.github.io/" text="tldr-pages.github.io" /></p>
-      <h3> Do you have any unwanted pieces of trivia for me? </h3> 
+      <h3> Do you have any unwanted pieces of trivia for me? </h3>
       <p> Well, this small app was built with ES6, type-checked using FlowType, the amazing reactivity is provided by RxJS, and all the rendering is done with React.</p>
       <p>Have a <Link href="https://github.com/ostera/tldr.jsx" text="look inside" /> and feel free to <Link href="https://github.com/ostera/tldr.jsx/fork" text="fork" /> </p>
   </section>
