c13b53a47b5a5438856bfd650300345946318894
Leandro Ostera <leandro@ostera.io>
2016-07-03 01:24:35 +0200

    Copy fixes for the Welcome and NotFound pages

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -11,8 +11,11 @@ export default props => (
       <Tldr size="small"/> is a community effort, we need people like you to raise the bar and
       find missing commands, suggest editions, and propose new pages.
     </p>
+    <h3> How can I help? </h3>
     <p>
-      Take a look at the open <Link href="https://github.com/tldr-pages/tldr/issues?q=is%3Aopen+is%3Aissue+label%3Acommand" text="Command Requests" /> or join in on any of the open <Link href="https://github.com/tldr-pages/tldr/pulls" text="command proposals" />.
+      Take a look at the open <Link href="https://github.com/tldr-pages/tldr/issues?q=is%3Aopen+is%3Aissue+label%3Acommand" text="Command Requests" /> to throw a jab at things people need, or maybe join in on any of the open <Link href="https://github.com/tldr-pages/tldr/pulls" text="command proposals" />.
+    </p>
+    <p> If the command you want hasn't been proposed yet, feel encouraged to submit a proposal yourself! 😉 &mdash; <Link href="https://github.com/tldr-pages/tldr/blob/master/CONTRIBUTING.md" text="Start here" />
     </p>
   </section>
 )
