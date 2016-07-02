19874473451bfe51ab7a40246c51b1e69426b191
Leandro Ostera <leandro@ostera.io>
2016-07-03 01:44:56 +0200

    Adds EDIT link on TldrPage

--- src/components/TldrPage.js
+++ src/components/TldrPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import marked from 'marked'
 
 import type { Page } from '../Page'
 
-import marked from 'marked'
+import Link from './Link'
 
 export default ({body, path}: Page) => (
-  <section className="content page" dangerouslySetInnerHTML={{__html: marked(body)}}>
+  <section className="content" >
+    <section className="page" dangerouslySetInnerHTML={{__html: marked(body)}}></section>
+    <Link href={path} text="Edit this page on Github" />
   </section>
 )
