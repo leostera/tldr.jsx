169cb642b5bf0553d44e05f3d3e16db2bffa7586
Leandro Ostera <leandro@ostera.io>
2016-07-02 22:03:14 +0200

    Adds split navigation preserving query params

--- src/components.js
+++ src/components.js
@@ -7,39 +7,57 @@ import ReactDOM from 'react-dom'
 import marked from 'marked'
 
 const Debug = (props) =>
-  <pre> {JSON.stringify(props, null, "  ")} </pre>
+  <pre className="debug">
+    <h2>Debug:</h2>
+    {JSON.stringify(props, null, "  ")}
+  </pre>
 
-const Nav = ({history, command: {name}}) => (
+
+let navigate = (history: History) => (path: string) =>
+  history.push({...history.getCurrentLocation(), pathname: `/${path}`})
+
+const Nav = ({navigate}) => (
   <nav>
+    <a className="brand" href="/" />
     <input
       type="text"
-      onChange={ ({target: {value}}) => { history.push({pathname: `/${value}`}) } }
-      defaultValue={name}
+      onChange={ ({target: {value}}) => navigate(value) }
+      placeholder="Command name"
     />
   </nav>
 )
 
 const TldrPage = ({body, path}: Page) => (
-  <pre dangerouslySetInnerHTML={{__html: marked(body)}}></pre>
-)
-
-const NotFound = (props) => (
-  <h1> Oops! Page not found! </h1>
-)
-
-const TldrApp = (props) => (
-  <section>
-    <Nav {...props.state} />
-    { !props.found && <NotFound {...props} /> }
-    { props.found && !props.page.error && <TldrPage {...props.page} /> }
-    { props.found && props.page.error && <NotFound {...props} /> }
-    <Debug {...props} />
+  <section className="content" dangerouslySetInnerHTML={{__html: marked(body)}}>
   </section>
 )
 
-export default (opts) => {
+const NotFound = which => props => (
+  <section className="content">
+    <h1> Oops! {which} not found! </h1>
+    <p> We looked and looked for it, but it's nowhere. Maybe you can help us find it? </p>
+  </section>
+)
+
+const CommandNotFound = NotFound("Command")
+const PageNotFound = NotFound("Page")
+
+const Welcome = () => (
+  <section className="content">
+      <h1>Welcome!</h1>
+  </section>
+)
+
+export default (props) => {
+  let {found, page, state: {debug, command, history}} = props
   ReactDOM.render((
-    <TldrApp {...opts} />
+    <section>
+      <Nav navigate={navigate(history)} />
+      { !command.name && <Welcome /> }
+      { command.name && !found && <CommandNotFound /> }
+      { found && page && !page.error && <TldrPage {...page} /> }
+      { found && page &&  page.error && <PageNotFound /> }
+      { debug && <Debug {...props} /> }
+    </section>
   ), document.getElementById('tldr'))
 }
-
