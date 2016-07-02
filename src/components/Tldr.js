4936977e8b132e2b859e05db870ebea7e6fa1b45
Leandro Ostera <leandro@ostera.io>
2016-07-03 01:23:54 +0200

    Logo links to home

--- src/components/Tldr.js
+++ src/components/Tldr.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Tldr = ({size}) => {
   let className=`brand ${size}`
-  return (<a className={className} href="//tldr-pages.github.io"/>)
+  return (<a className={className} href="/"/>)
 }
 
 export default Tldr
