ab9bb06ee8d6fdc3366baf5aa48b5e9c095e9cb9
Leandro Ostera <leandro@ostera.io>
2016-07-03 12:49:15 +0200

    Close #35

--- src/components/Debug.js
+++ src/components/Debug.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export default (props) =>
-  <pre className="debug">
-    <h2>Debug:</h2>
-    {JSON.stringify(props, null, "  ")}
-  </pre>
+import { parse, stringify } from 'query-string'
+
+export default (props) => {
+  let { state: { history } } = props
+
+  let close = (e) => {
+    let location = history.getCurrentLocation()
+    let search = parse(location.search)
+    delete search.debug
+    location.search = stringify(search)
+    history.push(location)
+  }
+
+  return (
+    <pre className="debug">
+      <span onClick={close}>✕</span>
+      <h2>Debug:</h2>
+      {JSON.stringify(props, null, "  ")}
+    </pre>
+  )
+}
