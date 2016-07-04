c811287138f17f41b9ed1ca1d73cd28361beb214
Leandro Ostera <leandro@ostera.io>
2016-07-04 19:00:19 +0200

    Type markdown props

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -2,13 +2,20 @@
 
 import React from 'react'
 import DOM from 'react-dom/server'
+
 import marked from 'marked'
 
 export const Cr = "\n\n"
 
-export const Markdown = (props) => {
+type MarkdownProps = {
+  className?: string;
+  body?: string;
+  children?: Object | [];
+}
+
+export const Markdown = (props: MarkdownProps) => {
   let { className, body, children } = props
-  if (body === undefined || body === null && children && children.length > 0) {
+  if ((body === undefined || body === null) && children && children.length > 0) {
     body = children
       .map( c => React.isValidElement(c) && DOM.renderToString(c) || c)
       .join('')
