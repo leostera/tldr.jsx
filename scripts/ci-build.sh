080574cefd325e1a99dd5eb52d469ce9fbe3060a
Leandro Ostera <leandro@ostera.io>
2016-07-15 12:53:43 +0200

    branch/ref/timestamp-ed build artifacts

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -2,10 +2,12 @@
 
 readonly IMAGE=quay.io/travisci/travis-node-js
 
+readonly STAMP=$(git rev-parse --abbrev-ref HEAD)@$(git rev-parse HEAD).$(date +%s)
+
 docker run \
   -ti \
   --entrypoint=/opt/build.sh \
-  -v $(pwd)/.travis_build:/root/build \
+  -v $(pwd)/.travis_build/${STAMP}:/root/build \
   -v ${HOME}/.ssh/id_rsa:/root/.ssh/id_rsa \
   -v $(pwd)/build/ci.sh:/opt/build.sh \
   ${IMAGE}
